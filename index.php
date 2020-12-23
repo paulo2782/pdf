d81c4cc2f29f250ab1ed4f6afefb3cb8eabcf824
Joao Manuel De Carlo <joao.carlo@controlle.com>
2020-12-23 09:25:51 -0300

    add layout in card

--- index.php
+++ index.php
@@ -6,18 +6,27 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<title>Converter PDF/CSV</title>
 	</head>
-	<body>
+	<body>		
 		<div class="container">
-			<h3>Converter PDF/CSV</h3>
-			<form name="PDF" action="convert.php" method="POST" enctype="multipart/form-data">
-				<label for="file"> Selecione o arquivo </label>
-				<label class="file">
-					<input type="file" id="file" name="file" class="form-control-file" accept="application/pdf">
-					<span class="file-custom"></span>
-				</label>
-				<input type="submit" value="Converter" class="form-control btn btn-primary"> 
-			</form>
+			<div class="mx-auto my-4">
+				<div class="card">
+					<div class="card-header">
+						<h3>Converter PDF/CSV</h3>
+					</div>
+					<div class="card-body">
+						<form name="PDF" action="convert.php" method="POST" enctype="multipart/form-data">
+							<label for="file"> Selecione o arquivo </label>
+							<label class="file">
+								<input type="file" id="file" name="file" class="form-control-file" accept="application/pdf">
+								<span class="file-custom"></span>
+							</label>
+							<input type="submit" value="Converter" class="form-control btn btn-primary"> 
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
+		
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
